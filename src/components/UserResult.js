09ecf07ad3b521910af5940476384f5e11165420
Amarjeet Singh Rai <jatt27@gmail.com>
2017-12-13 12:12:29 +0000

    Show more details for users

--- src/components/UserResult.js
+++ src/components/UserResult.js
@@ -13,16 +13,28 @@ const getLevelName = id => {
   }
 }
 
+const getProfileImageUrl = (user) => {
+  if (user.profileImage === 'NULL') {
+    return 'https://semantic-ui.com/images/avatar/large/jenny.jpg'
+  } else {
+    return `https://images3.the-dots.com/${user.profileImage}?p=profileImage`
+  }
+}
+
 export default ({user}) => (
   <div className='item'>
-    <a className='ui tiny image'><img src='https://semantic-ui.com/images/avatar/large/jenny.jpg'/></a>
+    <a className='ui tiny image'><img src={getProfileImageUrl(user)}/></a>
     <div className='content'>
       <a className='header' href={`https://the-dots.com/users/${user.id}`}>{user.firstName} {user.lastName}</a> - ({user.score})
+      <div className='meta'>
+        <span>{user.tagline}</span>
+      </div>
       <div className='description'>
         <p>Level: {getLevelName(user.level)}</p>
         <p>Professions: {user.professions.join(', ')}</p>
         <p>Skills: {user.skills.join(', ')}</p>
       </div>
+      <div className='extra'>{user.locationName}</div>
     </div>
   </div>
 )
